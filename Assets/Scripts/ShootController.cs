e7bda0977fc0bba4d5ca5105031a024b0c602e6b
Nameyho <mathieuallard1993@hotmail.com>
2021-04-16 11:17:10 +0200

    add bullet BulletTrail

--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -13,6 +13,8 @@ public class ShootController : MonoBehaviour
 
     private bool _isHit;
 
+    public TrailRenderer bulletTrailPrefab;
+
     private void Start() {
         _transform = transform;
     }
@@ -23,14 +25,19 @@ public class ShootController : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
              MuzzleFlashe.Play();
-
+             TrailRenderer bullletTrail = Instantiate(bulletTrailPrefab,_transform.position,Quaternion.identity);
+             bullletTrail.AddPosition(_transform.position);
              if(_isHit){
                 // faire des trucs avec la collission détéctée
                 ParticleSystem collissionExplosion = Instantiate(CollissionExplosionPrefab,_hitInfo.point,Quaternion.identity);
+                
+
                 //collissionExplosion.transform.LookAt(hitInfo.normal);
                 collissionExplosion.transform.rotation =   Quaternion.LookRotation(_hitInfo.normal,Vector3.up);
                 //Destroy(hitInfo.collider.gameObject);
                Destroy(collissionExplosion.gameObject,1f);
+               bullletTrail.transform.position = _hitInfo.point;
+               //bullletTrail.AddPosition(_hitInfo.point);
                }
         }
     }
