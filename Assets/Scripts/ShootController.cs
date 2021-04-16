2321e69cbd8b2f81fb8246a17c4af1bdcefeac0a
Nameyho <mathieuallard1993@hotmail.com>
2021-04-16 10:33:47 +0200

    add normal gizmo

--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -6,16 +6,39 @@ public class ShootController : MonoBehaviour
 {
     public ParticleSystem MuzzleFlashe;
     public ParticleSystem CollissionExplosionPrefab;
+
+    private Transform _transform;
+
+    private RaycastHit _hitInfo;
+
+    private bool _isHit;
+
+    private void Start() {
+        _transform = transform;
+    }
     private void Update() {
-        if(Input.GetMouseButtonDown(0)){
+
+        _isHit = Physics.Raycast(_transform.position,_transform.forward, out _hitInfo);
+
+        if(Input.GetMouseButtonDown(0))
+        {
              MuzzleFlashe.Play();
-            if(Physics.Raycast(transform.position,transform.forward, out RaycastHit hitInfo)){
+
+             if(_isHit){
                 // faire des trucs avec la collission détéctée
-                ParticleSystem collissionExplosion = Instantiate(CollissionExplosionPrefab,hitInfo.point,Quaternion.identity);
-                collissionExplosion.transform.LookAt(hitInfo.normal);
+                ParticleSystem collissionExplosion = Instantiate(CollissionExplosionPrefab,_hitInfo.point,Quaternion.identity);
+                //collissionExplosion.transform.LookAt(hitInfo.normal);
+                collissionExplosion.transform.rotation =   Quaternion.LookRotation(_hitInfo.normal,Vector3.up);
                 //Destroy(hitInfo.collider.gameObject);
                Destroy(collissionExplosion.gameObject,1f);
-            }
+               }
         }
     }
+
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(_hitInfo.point,_hitInfo.point + _hitInfo.normal);
+    }
+
+    
 }
